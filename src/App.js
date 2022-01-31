2d6fa3eb8540386f6bbfeda4f25b39e1f1b9723c
Paul Mougin <mougin.paul@gmail.com>
2022-01-31 14:47:27 +0100

    Initialisation du fichier lorem-ipsum

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ background-color: #C4C4C4 }}>
+      <h1>Lorem</h1>
+      <p>
+        Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at
+        eget metus. Cum sociis natoque penatibus et magnis dis parturient
+        montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Nullam id dolor id nibh ultricies vehicula ut id elit.
+        Nullam id dolor id nibh ultricies vehicula ut id elit. Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit. Nullam id dolor id nibh ultricies
+        vehicula ut id elit. Duis mollis, est non commodo luctus, nisi erat
+        porttitor ligula, eget lacinia odio sem nec elit. Duis mollis, est non
+        commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec
+        elit. Nullam quis risus eget urna mollis ornare vel eu leo. Nullam quis
+        risus eget urna mollis ornare vel eu leo. Praesent commodo cursus magna,
+        vel scelerisque nisl consectetur et.
+      </p>
     </div>
   );
 }
